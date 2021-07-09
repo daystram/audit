303d7568637b29d97528095258d16f19fb9c860b
Danny August Ramaputra <daystram@gmail.com>
2021-07-09 18:37:59 +1000

    chore: added rollup visualizer

--- audit-fe/vite.config.js
+++ audit-fe/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { createVuePlugin } from "vite-plugin-vue2";
 import ViteComponents, { VuetifyResolver } from "vite-plugin-components";
 import path from "path";
+import visualizer from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
@@ -10,6 +11,7 @@ export default defineConfig({
       globalComponentsDeclaration: true,
       customComponentResolvers: [VuetifyResolver()],
     }),
+    visualizer(),
   ],
   server: {
     port: 8080,
