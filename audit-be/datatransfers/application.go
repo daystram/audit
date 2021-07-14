7cff607b04fe44ed0200605911bd220b0846013d
Danny August Ramaputra <daystram@gmail.com>
2021-07-14 19:04:50 +1000

    refactor: added application and service df

---
+++ audit-be/datatransfers/application.go
@@ -0,0 +1,9 @@
+package datatransfers
+
+type ApplicationInfo struct {
+	ID          string `json:"id,omitempty" binding:"-"`
+	Name        string `json:"name" binding:"required"`
+	Description string `json:"description" binding:"required"`
+	CreatedAt   int64  `json:"createdAt,omitempty" binding:"-"`
+	UpdatedAt   int64  `json:"updatedAt,omitempty" binding:"-"`
+}
