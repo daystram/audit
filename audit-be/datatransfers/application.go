ce3f72c3e31c0416136bd9d7e164d0d7c91cc707
Danny August Ramaputra <daystram@gmail.com>
2021-07-14 19:31:33 +1000

    feat: added  datatransfers

--- audit-be/datatransfers/application.go
+++ audit-be/datatransfers/application.go
@@ -1,9 +1,11 @@
 package datatransfers
 
 type ApplicationInfo struct {
-	ID          string `json:"id,omitempty" binding:"-"`
+	ID string `json:"id,omitempty" binding:"-"`
+
 	Name        string `json:"name" binding:"required"`
 	Description string `json:"description" binding:"required"`
 	CreatedAt   int64  `json:"createdAt,omitempty" binding:"-"`
-	UpdatedAt   int64  `json:"updatedAt,omitempty" binding:"-"`
+
+	UpdatedAt int64 `json:"updatedAt,omitempty" binding:"-"`
 }
