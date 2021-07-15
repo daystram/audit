c6d2dc86ea0306110233c9ca1f9753287c79c85d
Danny August Ramaputra <daystram@gmail.com>
2021-07-15 18:46:52 +1000

    feat: implemented application models

--- audit-be/models/application.go
+++ audit-be/models/application.go
@@ -18,9 +18,39 @@ type Application struct {
 }
 
 type ApplicationOrmer interface {
+	GetAll() (applications []Application, err error)
+	GetOneByID(ID string) (application Application, err error)
+	Insert(application Application) (ID string, err error)
+	Update(application Application) (err error)
+	DeleteByID(ID string) (err error)
 }
 
 func NewApplicationOrmer(db *gorm.DB) ApplicationOrmer {
 	_ = db.AutoMigrate(&Application{})
 	return &applicationOrm{db}
+}
+
+func (o *applicationOrm) GetAll() (applications []Application, err error) {
+	result := o.db.Model(&Application{}).Find(&applications)
+	return applications, result.Error
+}
+
+func (o *applicationOrm) GetOneByID(ID string) (application Application, err error) {
+	result := o.db.Model(&Application{}).Where("ID = ?", ID).First(&application)
+	return application, result.Error
+}
+
+func (o *applicationOrm) Insert(application Application) (ID string, err error) {
+	result := o.db.Model(&Application{}).Create(&application)
+	return application.ID, result.Error
+}
+
+func (o *applicationOrm) Update(application Application) (err error) {
+	result := o.db.Model(&Application{}).Where("ID = ?", application.ID).Updates(&application)
+	return result.Error
+}
+
+func (o *applicationOrm) DeleteByID(ID string) (err error) {
+	result := o.db.Model(&Application{}).Where("ID = ?", ID).Delete(Application{})
+	return result.Error
 }
