1a67da64030ade962de7177c705f42beeef62e75
Danny August Ramaputra <daystram@gmail.com>
2021-07-16 11:14:15 +1000

    feat: added application api routes

--- audit-be/router/router.go
+++ audit-be/router/router.go
@@ -2,9 +2,22 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+
+	v1 "github.com/daystram/audit/audit-be/controllers/v1"
 )
 
 func InitializeRouter() (router *gin.Engine) {
 	router = gin.Default()
+	apiV1 := router.Group("/api/v1")
+	{
+		application := apiV1.Group("/application")
+		{
+			application.GET("/", v1.GETApplicationList)
+			application.POST("/", v1.POSTApplicationCreate)
+			application.GET("/:application_id", v1.GETApplicationDetail)
+			application.PUT("/:application_id", v1.PUTApplicationUpdate)
+			application.DELETE("/:application_id", v1.DELETEApplicationDelete)
+		}
+	}
 	return
 }
