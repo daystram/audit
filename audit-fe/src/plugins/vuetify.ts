863adcf0cf46572adbfbdcaf99383af308df50ec
Danny August Ramaputra <daystram@gmail.com>
2021-07-19 08:59:17 +1000

    refactor: configured color themes

--- audit-fe/src/plugins/vuetify.ts
+++ audit-fe/src/plugins/vuetify.ts
@@ -3,4 +3,19 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: "#4C1ED6",
+        primaryDim: "#4721B8",
+        secondary: "#D8315B",
+      },
+    },
+    options: { customProperties: true },
+  },
+  icons: {
+    iconfont: "mdi",
+  },
+});
