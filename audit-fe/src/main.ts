797fe630bb2d8b8399827755b72a2275a2b50c7f
Danny August Ramaputra <daystram@gmail.com>
2021-07-19 22:02:40 +1000

    feat: added page load status mixin

--- audit-fe/src/main.ts
+++ audit-fe/src/main.ts
@@ -3,11 +3,12 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import { StatusMixin } from "./constants/status";
 
 import "@/styles/App.sass";
 
 Vue.config.productionTip = false;
-
+Vue.mixin(StatusMixin);
 new Vue({
   router,
   vuetify,
