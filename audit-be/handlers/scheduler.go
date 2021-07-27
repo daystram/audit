0353d0399e816011edc4c904b53e96695215d5ed
Danny August Ramaputra <daystram@gmail.com>
2021-07-27 14:57:52 +1000

    refactor: fixed tracking cron trigger on no services

--- audit-be/handlers/scheduler.go
+++ audit-be/handlers/scheduler.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/robfig/cron/v3"
+	"gorm.io/gorm"
 
 	"github.com/daystram/audit/audit-be/constants"
 	"github.com/daystram/audit/audit-be/models"
@@ -24,7 +25,7 @@ func (m *module) InitializeScheduler() (err error) {
 func (m *module) TriggerTracking() {
 	var err error
 	var services []models.Service
-	if services, err = m.db.serviceOrmer.GetAllEnabled(); err != nil {
+	if services, err = m.db.serviceOrmer.GetAllEnabled(); err != nil && err != gorm.ErrRecordNotFound {
 		log.Println(err)
 		return
 	}
